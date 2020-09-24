2a1ede89c4fcc48650114590f5418378b9562aee
Cerberus <cerberusprogrammer@gmail.com>
2020-09-24 10:15:31 -0700

    this

---
+++ clase/pilas/pila.java
@@ -0,0 +1,8 @@
+package pilas;
+
+public class Pila {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ practica-1/src/Pila.java
@@ -0,0 +1,40 @@
+package src;
+
+public class Pila<T> {
+
+    int top;
+    T[] pila;
+
+    void push(T object){
+
+    }
+
+    T pop(){
+        T data;
+        if (isEmpty())
+            return null;
+        else {
+            data = pila[top];
+            top--;
+        }
+        return data;
+    }
+
+    boolean isFull(){
+        return top == 10;
+    }
+
+    boolean isEmpty(){
+        return top == -1;
+    }
+    }
+
+    T peek(){
+        return pila[top];
+    }
+
+
+    public static void main(String[] args) {
+        //Pila pila = (T[]) new Object[10];
+    }
+}
