1e6e37d4682b103a8e5c8b129f0162da7ebafaf1
Cerberus <cerberusprogrammer@gmail.com>
2020-09-23 18:21:41 -0700

    Functions

--- practica-1/src/Controller.java
+++ practica-1/src/Controller.java
@@ -1,4 +1,25 @@
 package src;
 
 public class Controller {
+
+    public void drawSquare() {
+        
+    }
+
+    public void drawTriangle() {
+        
+    }
+
+    public void drawCircle() {
+        
+    }
+
+    public void drawSpiral() {
+
+    }
+
+    public void drawPolygon() {
+        
+    }
+
 }
